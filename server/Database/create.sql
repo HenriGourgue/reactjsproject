fbf0d81390f2d8ff2382d73ca4425982271fe973
Henri Gourgue <gourguehenri@gmail.com>
2021-04-26 20:20:25 +0200

    Sql create schema script

---
+++ server/Database/create.sql
@@ -0,0 +1,28 @@
+truncate table favorite;
+drop table if exists favorite;
+
+truncate table user;
+drop table if exists user;
+
+CREATE DATABASE IF NOT EXISTS beer;
+
+CREATE TABLE IF NOT EXISTS user(
+    id MEDIUMINT not null AUTO_INCREMENT,
+    name varchar(100),
+    email varchar(100),
+    password varchar(200),
+    primary key(id)
+);
+
+CREATE TABLE IF NOT EXISTS favorite(
+    id MEDIUMINT not null AUTO_INCREMENT,
+    name varchar(100),
+    abv decimal(5,2),
+    image varchar(400),
+    description longtext,
+    tag varchar(400),
+    userId MEDIUMINT,
+    date varchar(50),
+    primary key(id),
+    foreign key(userId) references user(id)
+);
